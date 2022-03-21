9e7f2c6b656a9e7578458d969c9201781bd538d0
Rohit Maheshwari <rrohit.maheshwari@gmail.com>
2022-03-21 15:30:41 -0700

    Create evalRPN.java

---
+++ src/leetcodeProblems/evalRPN.java
@@ -0,0 +1,53 @@
+/*
+150. Evaluate Reverse Polish Notation
+https://leetcode.com/problems/evaluate-reverse-polish-notation/
+https://leetcode.com/explore/interview/card/linkedin/336/heap-queue-stack/1959/
+*/
+
+class Solution {
+    public int evalRPN(String[] tokens) {
+        
+        Stack<String> st = new Stack<>();
+        int result;
+        for(String s:tokens)
+        {
+            result=0;
+            if(isNumeric(s))
+            {
+                st.push(s);
+            }
+            else
+            {
+                if(s.equals("+"))
+                {
+                   result =   Integer.parseInt(st.pop())+Integer.parseInt(st.pop());
+                }
+                else if(s.equals("-"))
+                {
+                   result =  - Integer.parseInt(st.pop())+ Integer.parseInt(st.pop());
+                }
+                 else if(s.equals("/"))
+                {
+                     result = Integer.parseInt(st.pop());
+                   result = Integer.parseInt(st.pop())/result;
+                }
+                 else if(s.equals("*"))
+                {
+                   result =   Integer.parseInt(st.pop())*Integer.parseInt(st.pop());
+                }
+                 st.push(result+"");
+            }
+        }
+        return Integer.parseInt(st.pop());
+        
+    }
+    
+    public static boolean isNumeric(String str) { 
+      try {  
+        Integer.parseInt(str);  
+        return true;
+      } catch(Exception e){  
+        return false;  
+      }  
+}
+}
