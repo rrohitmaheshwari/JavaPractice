f9a79016e5bd91fcf6432348c4220e6a9bdfb518
Rohit Maheshwari <rrohit.maheshwari@gmail.com>
2022-03-23 00:09:38 -0700

    Create findCelebrity.java

---
+++ src/leetcodeProblems/findCelebrity.java
@@ -0,0 +1,37 @@
+/*
+277. Find the Celebrity
+https://leetcode.com/problems/find-the-celebrity/
+https://leetcode.com/explore/interview/card/linkedin/339/array-and-strings/2909/
+
+O(N)
+O(1)
+*/
+
+public class Solution extends Relation {
+    
+    private int numberOfPeople;
+    
+    public int findCelebrity(int n) {
+        numberOfPeople = n;
+        int celebrityCandidate = 0;
+        for (int i = 0; i < n; i++) {
+            if (knows(celebrityCandidate, i)) {
+                celebrityCandidate = i;
+            }
+        }
+        if (isCelebrity(celebrityCandidate)) {
+            return celebrityCandidate;
+        }
+        return -1;
+    }
+    
+    private boolean isCelebrity(int i) {
+        for (int j = 0; j < numberOfPeople; j++) {
+            if (i == j) continue; // Don't ask if they know themselves.
+            if (knows(i, j) || !knows(j, i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
