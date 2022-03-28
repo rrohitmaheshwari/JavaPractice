967d5be342f39c4852adc85ee5d8e1fc171196b6
Rohit Maheshwari <rrohit.maheshwari@gmail.com>
2022-03-28 13:51:12 -0700

    Create searchMatrix.java

---
+++ src/leetcodeProblems/searchMatrix.java
@@ -0,0 +1,24 @@
+/*
+ Search a 2D Matrix II
+https://leetcode.com/problems/inorder-successor-in-bst/solution/
+ */
+
+ class Solution {
+   public boolean searchMatrix(int[][] matrix, int target) {
+        // start our "pointer" in the bottom-left
+        int row = matrix.length-1;
+        int col = 0;
+
+        while (row >= 0 && col < matrix[0].length) {
+            if (matrix[row][col] > target) {
+                row--;
+            } else if (matrix[row][col] < target) {
+                col++;
+            } else { // found it
+                return true;
+            }
+        }
+
+        return false;
+    }
+}'
