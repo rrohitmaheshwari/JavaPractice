14147e095776ed685bd04654192943b246d48865
Rohit Maheshwari <rrohit.maheshwari@gmail.com>
2022-03-28 23:32:02 -0700

    Create happyNumber.java

---
+++ src/leetcodeProblems/happyNumber.java
@@ -0,0 +1,28 @@
+/*
+202. Happy Number
+https://leetcode.com/problems/happy-number/
+O(logN)
+O(1) - not maintinaing hash map
+*/
+
+class Solution {
+     public int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n = n / 10;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+
+    public boolean isHappy(int n) {
+        int slowRunner = n;
+        int fastRunner = getNext(n);
+        while (fastRunner != 1 && slowRunner != fastRunner) {
+            slowRunner = getNext(slowRunner);
+            fastRunner = getNext(getNext(fastRunner));
+        }
+        return fastRunner == 1;
+    }
+}
