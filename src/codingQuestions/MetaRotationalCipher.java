d9fd0314a9b387da5e72b4e9b963dfc0ab0a08a3
Rohit Maheshwari <rrohit.maheshwari@gmail.com>
2022-04-08 23:06:24 -0700

    Update MetaRotationalCipher.java

--- src/codingQuestions/MetaRotationalCipher.java
+++ src/codingQuestions/MetaRotationalCipher.java
@@ -7,6 +7,15 @@ Given a string and a rotation factor, return an encrypted string.
 */
 
 
+import java.util.*;
+// Add any extra import statements you may need here
+
+
+class Main {
+
+  // Add any helper functions you may need here
+  
+
   String rotationalCipher(String input, int rotationFactor) {
     // Write your code here
     StringBuilder sb= new StringBuilder();
@@ -33,3 +42,58 @@ Given a string and a rotation factor, return an encrypted string.
     }
     return sb.toString();
   }
+
+
+
+
+
+
+
+
+
+
+
+  // These are the tests we use to determine if the solution is correct.
+  // You can add your own at the bottom.
+  int test_case_number = 1;
+  void check(String expected, String output) {
+    boolean result = (expected.equals(output));
+    char rightTick = '\u2713';
+    char wrongTick = '\u2717';
+    if (result) {
+      System.out.println(rightTick + " Test #" + test_case_number);
+    }
+    else {
+      System.out.print(wrongTick + " Test #" + test_case_number + ": Expected ");
+      printString(expected); 
+      System.out.print(" Your output: ");
+      printString(output);
+      System.out.println();
+    }
+    test_case_number++;
+  }
+  void printString(String str) {
+    System.out.print("[\"" + str + "\"]");
+  }
+  
+  public void run() {
+    String input_1 = "All-convoYs-9-be:Alert1.";
+    int rotationFactor_1 = 4;
+    String expected_1 = "Epp-gsrzsCw-3-fi:Epivx5.";
+    String output_1 = rotationalCipher(input_1, rotationFactor_1);
+    check(expected_1, output_1);
+
+    String input_2 = "abcdZXYzxy-999.@";
+    int rotationFactor_2 = 200;
+    String expected_2 = "stuvRPQrpq-999.@";
+    String output_2 = rotationalCipher(input_2, rotationFactor_2);
+    check(expected_2, output_2);
+
+    // Add your own test cases here
+    
+  }
+  
+  public static void main(String[] args) {
+    new Main().run();
+  }
+}
