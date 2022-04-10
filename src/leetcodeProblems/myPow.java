924704372563eb08cf5642d2c06d7f5e6d7ff6d9
Rohit Maheshwari <rrohit.maheshwari@gmail.com>
2022-04-09 20:24:16 -0700

    Update myPow.java

--- src/leetcodeProblems/myPow.java
+++ src/leetcodeProblems/myPow.java
@@ -23,3 +23,38 @@ class Solution {
       return n >= 0 ? ans : 1 / ans;
     }
 }
+
+/*
+recursion approach
+O(log N)
+O(log N) 
+*/
+
+class Solution {
+    public double myPow(double x, int n) {
+        long N = n;  //casting it to long just to be safe -n -> n fails when n is MIN_INT
+        
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        
+        return helperPow(x,N);
+    }
+    
+    double helperPow(double x,Long n)  // space O(log N) stacks (could also be reduced to O(1) if we follow iterative approach)
+    {
+        if(n==0L)
+            return 1D;
+        if(n==1L)
+            return x;
+        
+        double ans = helperPow(x,n/2);  // time complexity O(log N)
+        ans = ans *ans;
+        if(n%2 == 1)
+            ans = ans * x;
+        
+        return ans;    
+    }
+    
+}
