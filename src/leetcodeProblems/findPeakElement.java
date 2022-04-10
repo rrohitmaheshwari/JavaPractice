5e41880bb1e44b43b7f8d90d54f2dfed65ea7595
Rohit Maheshwari <rrohit.maheshwari@gmail.com>
2022-04-10 15:15:55 -0700

    Update findPeakElement.java

--- src/leetcodeProblems/findPeakElement.java
+++ src/leetcodeProblems/findPeakElement.java
@@ -2,18 +2,23 @@
 162. Find Peak Element
 https://leetcode.com/problems/find-peak-element/
 */
-
 class Solution {
     public int findPeakElement(int[] nums) {
                int l = 0, r = nums.length - 1;
-        while (l < r) {
+        int ans=l;
+        while (l <= r) {
             int mid = (l + r) / 2;
-            if (nums[mid] > nums[mid + 1])
-                r = mid;
+            if (mid+1<nums.length && nums[mid] <= nums[mid + 1])
+            {
+                ans = mid+1;
+                l = mid+1;
+            }  
             else
-                l = mid + 1;
+            {
+                 r = mid - 1;
+            }   
         }
-        return l;
+        return ans;
         
     }
 }
