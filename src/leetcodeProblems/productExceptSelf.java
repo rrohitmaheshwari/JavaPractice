db5c5b8473ce410340e354f15c5cc25823ca62e4
Rohit Maheshwari <rrohit.maheshwari@gmail.com>
2022-04-11 21:18:31 -0700

    Update productExceptSelf.java

--- src/leetcodeProblems/productExceptSelf.java
+++ src/leetcodeProblems/productExceptSelf.java
@@ -2,7 +2,7 @@
 238. Product of Array Except Self
 https://leetcode.com/problems/product-of-array-except-self/
 O(N)
-O(N)
+O(N)....O(1) solution with resusing same array
 */
 
 class Solution {
@@ -35,8 +35,46 @@ class Solution {
         {
             res[i] = left[i-1] * right[i+1];
         }
-        
+
         return res;
         
+    }
+}
+
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+
+        // The length of the input array 
+        int length = nums.length;
+
+        // Final answer array to be returned
+        int[] answer = new int[length];
+
+        // answer[i] contains the product of all the elements to the left
+        // Note: for the element at index '0', there are no elements to the left,
+        // so the answer[0] would be 1
+        answer[0] = 1;
+        for (int i = 1; i < length; i++) {
+
+            // answer[i - 1] already contains the product of elements to the left of 'i - 1'
+            // Simply multiplying it with nums[i - 1] would give the product of all 
+            // elements to the left of index 'i'
+            answer[i] = nums[i - 1] * answer[i - 1];
+        }
+
+        // R contains the product of all the elements to the right
+        // Note: for the element at index 'length - 1', there are no elements to the right,
+        // so the R would be 1
+        int R = 1;
+        for (int i = length - 1; i >= 0; i--) {
+
+            // For the index 'i', R would contain the 
+            // product of all elements to the right. We update R accordingly
+            answer[i] = answer[i] * R;
+            R *= nums[i];
+        }
+
+        return answer;
     }
 }
