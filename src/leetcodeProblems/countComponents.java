d06876b298eddaa975f174c6f9ca4d369638c92d
Rohit Maheshwari <rrohit.maheshwari@gmail.com>
2022-04-18 00:48:18 -0700

    Update countComponents.java

--- src/leetcodeProblems/countComponents.java
+++ src/leetcodeProblems/countComponents.java
@@ -3,6 +3,8 @@
 https://leetcode.com/problems/number-of-connected-components-in-an-undirected-graph/
 O(N+E)
 O(N+E)
+
+BETER sol is union find as it uses less memory
 */
 
 class Solution {
@@ -47,4 +49,43 @@ class Solution {
              }
          }
      }
+}
+
+
+
+/*
+Union Find
+O(N+E)
+O(N)
+*/
+
+class Solution {
+    public int countComponents(int n, int[][] edges) {
+        int[] parent = new int[n];
+        int[] size = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+        int components = n;
+        for (int[] e : edges) {
+            int p1 = findParent(parent, e[0]);
+            int p2 = findParent(parent, e[1]);
+            if (p1 != p2) {
+                if (size[p1] < size[p2]) { // Merge small size to large size
+                    size[p2] += size[p1];
+                    parent[p1] = p2;
+                } else {
+                    size[p1] += size[p2];
+                    parent[p2] = p1;
+                }
+                components--;
+            }
+        }
+        return components;
+    }
+    private int findParent(int[] parent, int i) {
+        if (i == parent[i]) return i;
+        return parent[i] = findParent(parent, parent[i]); // Path compression
+    }
 }
