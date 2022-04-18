1de1158fbabe7b2e181734d169054a96bb94d127
Rohit Maheshwari <rrohit.maheshwari@gmail.com>
2022-04-17 23:37:43 -0700

    Update sparseVector.java

--- src/leetcodeProblems/sparseVector.java
+++ src/leetcodeProblems/sparseVector.java
@@ -27,3 +27,55 @@ class SparseVector {
 // SparseVector v1 = new SparseVector(nums1);
 // SparseVector v2 = new SparseVector(nums2);
 // int ans = v1.dotProduct(v2);
+
+//------------OPTIMIZED WAY
+
+
+/*
+Time complexity: 
+O(n)O(n) for creating the <index, value> pair for non-zero values; 
+O(L1+L2) for dot product
+
+Space complexity: 
+O(L) for creating the <index, value> pairs for non-zero values. 
+O(1)O(1) for calculating the dot product.
+*/
+
+class SparseVector {
+
+    List<int[]> sparseList;
+
+    SparseVector(int[] nums) {
+        sparseList = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) 
+        {
+            if (nums[i] != 0) 
+            {
+                sparseList.add(new int[]{i, nums[i]});
+            }
+        }
+    }
+
+    // Return the dotProduct of two sparse vectors
+    public int dotProduct(SparseVector vec) {
+        int result = 0, p = 0, q = 0;
+        while (p < sparseList.size() && q < vec.sparseList.size()) 
+        {
+            if (sparseList.get(p)[0] == vec.sparseList.get(q)[0]) 
+            {
+                result += sparseList.get(p)[1] * vec.sparseList.get(q)[1];
+                p++;
+                q++;
+            }
+            else if (sparseList.get(p)[0] > vec.sparseList.get(q)[0]) 
+            {
+                q++;
+            }
+            else 
+            {
+                p++;
+            }
+        }
+        return result;
+    }
+}
