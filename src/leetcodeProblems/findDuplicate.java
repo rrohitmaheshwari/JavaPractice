4b510282f380c16ec38d0911a48ae5d91fb8db40
Rohit Maheshwari <rrohit.maheshwari@gmail.com>
2022-04-20 21:31:49 -0700

    Create findDuplicate.java

---
+++ src/leetcodeProblems/findDuplicate.java
@@ -0,0 +1,50 @@
+/*
+287. Find the Duplicate Number
+https://leetcode.com/problems/find-the-duplicate-number/
+O(N)
+O(1)
+
+
+// easier solution is the negative marking
+*/
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        
+        // Find the intersection point of the two runners.
+        int tortoise = nums[0];
+        int hare = nums[0];
+        
+        do {
+            tortoise = nums[tortoise];
+            hare = nums[nums[hare]];
+        } while (tortoise != hare);
+
+        // Find the "entrance" to the cycle.
+        tortoise = nums[0];
+        
+        while (tortoise != hare) {
+            tortoise = nums[tortoise];
+            hare = nums[hare];
+        }
+
+        return hare;
+    }
+}
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int duplicate = -1;
+        for (int i = 0; i < nums.length; i++) {
+            int cur = Math.abs(nums[i]);
+            if (nums[cur] < 0) {
+                return cur;
+            }
+            nums[cur] *= -1;
+        }
+
+        // if required we can restore back the nums array 
+        
+        return duplicate;
+    }
+}
