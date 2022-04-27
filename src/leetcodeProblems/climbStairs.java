f48f5b4f4b74c95500f6d379bec147ed99a76c5e
Rohit Maheshwari <rrohit.maheshwari@gmail.com>
2022-04-27 16:42:15 -0700

    Update climbStairs.java

--- src/leetcodeProblems/climbStairs.java
+++ src/leetcodeProblems/climbStairs.java
@@ -28,3 +28,23 @@ class Solution {
         return memo[currstep];
     }
 }
+
+//Actual DP solution with O(1) memory
+class Solution {
+    public int climbStairs(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+
+        int prev1 = 1;
+        int prev2 = 2;
+
+        for (int i = 3; i <= n; i++) {
+            int newValue = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = newValue;
+        }
+
+        return prev2;
+    }
+}
