9fab008d2282ab72252607cbe42755d53fc6c4b2
Rohit Maheshwari <rrohit.maheshwari@gmail.com>
2022-04-28 02:22:38 -0700

    Create findTargetSumWays.java

---
+++ src/leetcodeProblems/findTargetSumWays.java
@@ -0,0 +1,41 @@
+/*
+494. Target Sum
+https://leetcode.com/problems/target-sum/
+O(tn) - >  t is sum of array and n is len of nums 
+O(tn) 
+
+Copied from Leetcode
+*/
+
+
+public class Solution {
+    int total;
+    
+    public int findTargetSumWays(int[] nums, int S) {
+        total = Arrays.stream(nums).sum();
+        
+        int[][] memo = new int[nums.length][2 * total + 1];  \\ can go from -target to target
+        for (int[] row : memo) {
+            Arrays.fill(row, Integer.MIN_VALUE);
+        }
+        return calculate(nums, 0, 0, S, memo);
+    }
+    
+    public int calculate(int[] nums, int i, int sum, int S, int[][] memo) {
+        if (i == nums.length) {
+            if (sum == S) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } else {
+            if (memo[i][sum + total] != Integer.MIN_VALUE) {
+                return memo[i][sum + total];
+            }
+            int add = calculate(nums, i + 1, sum + nums[i], S, memo);
+            int subtract = calculate(nums, i + 1, sum - nums[i], S, memo);
+            memo[i][sum + total] = add + subtract;
+            return memo[i][sum + total];
+        }
+    }
+}
