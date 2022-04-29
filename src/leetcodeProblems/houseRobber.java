718e17e9c4dc26cf6b4c34c56bf3c133844a84a0
Rohit Maheshwari <rrohit.maheshwari@gmail.com>
2022-04-29 15:12:37 -0700

    Update houseRobber.java

--- src/leetcodeProblems/houseRobber.java
+++ src/leetcodeProblems/houseRobber.java
@@ -39,3 +39,33 @@ class Solution {
         return ans;
     }
 }
+
+
+// DP solution
+class Solution {
+    
+    public int rob(int[] nums) {
+        
+        int N = nums.length;
+        
+        // Special handling for empty array case.
+        if (N == 0) {
+            return 0;
+        }
+        
+        int[] maxRobbedAmount = new int[nums.length + 1];
+        
+        // Base case initializations.
+        maxRobbedAmount[N] = 0;
+        maxRobbedAmount[N - 1] = nums[N - 1];
+        
+        // DP table calculations.
+        for (int i = N - 2; i >= 0; --i) {
+            
+            // Same as the recursive solution.
+            maxRobbedAmount[i] = Math.max(maxRobbedAmount[i + 1], maxRobbedAmount[i + 2] + nums[i]);
+        }
+        
+        return maxRobbedAmount[0];
+    }
+}
