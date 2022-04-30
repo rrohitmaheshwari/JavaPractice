e04c52a405f8bfb66d33cb712d448eb4e3e4aba9
Rohit Maheshwari <rrohit.maheshwari@gmail.com>
2022-04-29 20:57:47 -0700

    Create trimBST.java

---
+++ src/leetcodeProblems/trimBST.java
@@ -0,0 +1,33 @@
+/*
+669. Trim a Binary Search Tree
+https://leetcode.com/problems/trim-a-binary-search-tree/
+O(N)
+O(N) -> skewed tree call stack
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null) return root;
+        if (root.val > high) return trimBST(root.left, low, high);
+        if (root.val < low) return trimBST(root.right, low, high);
+
+        root.left = trimBST(root.left, low, high);
+        root.right = trimBST(root.right, low, high);
+        return root;       
+    }
+}
