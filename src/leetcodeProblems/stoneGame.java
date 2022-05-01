1fa1f9823ca8da99a5827c6f468d9cde72b54887
Rohit Maheshwari <rrohit.maheshwari@gmail.com>
2022-04-30 19:45:59 -0700

    Update stoneGame.java

--- src/leetcodeProblems/stoneGame.java
+++ src/leetcodeProblems/stoneGame.java
@@ -32,3 +32,25 @@ class Solution {
         return dp[l][r];
     }
 }
+
+
+// DP way
+
+ public boolean stoneGame(int[] p) {
+        int n = p.length;
+        int[][] dp  = new int[n][n];
+
+       // fill up the diagnonal
+        for (int i = 0; i < n; i++) 
+        	dp[i][i] = p[i];
+
+
+        // go diagonal wise
+        for (int d = 1; d < n; d++)
+            for (int i = 0; i < n - d; i++)
+                dp[i][i + d] = Math.max(p[i] - dp[i + 1][i + d]
+                	, p[i + d] - dp[i][i + d - 1]);
+
+
+        return dp[0][n - 1] > 0;
+    }
