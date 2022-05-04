d805cc8b0daea173f58e3430f75c7bd41191c99d
Rohit Maheshwari <rrohit.maheshwari@gmail.com>
2022-05-03 23:43:31 -0700

    Create combinationSum4.java

---
+++ src/leetcodeProblems/combinationSum4.java
@@ -0,0 +1,60 @@
+/*
+377. Combination Sum IV
+https://leetcode.com/problems/combination-sum-iv/
+O(T⋅N) -> T different input values and N is number of nums as that will be size of the tree
+O(N)
+
+copied from leetcode
+*/
+
+
+class Solution {
+    private HashMap<Integer, Integer> memo;
+
+    public int combinationSum4(int[] nums, int target) {
+        // minor optimization
+        // Arrays.sort(nums);
+        memo = new HashMap<>();
+        return combs(nums, target);
+    }
+
+    private int combs(int[] nums, int remain) {
+        if (remain == 0)
+            return 1;
+        if (memo.containsKey(remain))
+            return memo.get(remain);
+
+        int result = 0;
+        for (int num : nums) {
+            if (remain - num >= 0)
+                result += combs(nums, remain - num);
+            // minor optimizaton, early stopping
+            // else
+            //     break;
+        }
+        memo.put(remain, result);
+        return result;
+    }
+}
+
+// dp way
+class Solution {
+
+    public int combinationSum4(int[] nums, int target) {
+        // minor optimization
+        // Arrays.sort(nums);
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+
+        for (int combSum = 1; combSum < target + 1; ++combSum) {
+            for (int num : nums) {
+                if (combSum - num >= 0)
+                    dp[combSum] += dp[combSum - num];
+                // minor optimizaton, early stopping
+                // else
+                //     break;
+            }
+        }
+        return dp[target];
+    }
+}
